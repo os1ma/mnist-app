6f13bb32823515cbb00a99669648330fcee267e9
Yuki Oshima <39944763+os1ma@users.noreply.github.com>
2021-11-12 20:23:05 +0900

    Update frontend to show on table

--- nginx/html/main.js
+++ nginx/html/main.js
@@ -10,9 +10,29 @@ function escapeHTML(obj) {
 async function predict() {
   const response = await axios.post('/api/predict')
   const result = response.data.result
+  console.log(`result = ${result}`)
 
-  const resultElement = document.querySelector('#result')
-  resultElement.textContent = escapeHTML(result)
+  const tableBody = document.querySelector('#result-table-body')
+
+  while (tableBody.firstChild) {
+    tableBody.removeChild(tableBody.firstChild)
+  }
+
+  result.forEach((v, i) => {
+    const tr = document.createElement('tr')
+
+    // 数字
+    const tdMessage = document.createElement('td')
+    tdMessage.textContent = escapeHTML(i)
+    tr.appendChild(tdMessage)
+
+    // 確率
+    const tdCreatedAt = document.createElement('td')
+    tdCreatedAt.textContent = escapeHTML(v)
+    tr.appendChild(tdCreatedAt)
+
+    tableBody.appendChild(tr)
+  })
 }
 
 document.querySelector('#submit-button').addEventListener('click', predict)
