fac7d8a4eaaaafb49524e597b3941cb53cb0a81c
Yuki Oshima <39944763+os1ma@users.noreply.github.com>
2021-11-19 18:04:23 +0900

    Add run.sh

---
+++ bin/run.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+set -o errexit
+set -o nounset
+set -o pipefail
+set -o xtrace
+
+readonly SCRIPT_DIR="$(cd "$(dirname "$0")"; pwd)"
+readonly PROJECT_HOME="${SCRIPT_DIR}/.."
+
+cd "${PROJECT_HOME}/pytorch"
+docker-compose up
+
+cp "${PROJECT_HOME}/pytorch/model.onnx" "${PROJECT_HOME}/fastapi/model.onnx"
+
+cd "${PROJECT_HOME}"
+docker-compose up -d
