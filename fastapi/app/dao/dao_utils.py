e8eddf34dfd64a063bec62fab4f00b4567da91d3
Yuki Oshima <39944763+os1ma@users.noreply.github.com>
2021-11-26 18:46:58 +0900

    Add MySQL Connection class

---
+++ fastapi/app/dao/dao_utils.py
@@ -0,0 +1,18 @@
+import MySQLdb
+
+
+class MySQLConnection:
+    def __enter__(self):
+        self.con = MySQLdb.connect(
+            host='db',
+            db='app',
+            user='app',
+            passwd='password',
+            charset='utf8')
+
+        self.cur = self.con.cursor()
+        return self
+
+    def __exit__(self, exception_type, exception_value, traceback):
+        self.cur.close()
+        self.con.close()
